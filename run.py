7ad39ebb982725b82784e2729ae510c1dd5a7c99
serg <serg_kas@mail.ru>
2022-04-12 11:24:53 +0300

    tessaract for windows

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from PIL import Image
 import pytesseract
 from pytesseract import Output
 import cv2 as cv
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 
 chars_mask_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
